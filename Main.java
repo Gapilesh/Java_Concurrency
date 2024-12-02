e98cf28aceb0080a77d3dbda559cc9a8bd7d168f
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-02 14:11:53 +0530

    Created new Runnable class (method 2 of creating a thread)

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Creating a thread by creating the subclass of a thread class
+        // Creating a thread by creating the subclass of a thread class
         MyThread thread1 = new MyThread();      //Creating an instance of MyThread in the main class.
 
+        /* Method 2 - Create an instance of a class that implements the runnable interface
+           and then can pass the instance within the constructor of a thread object. */
+        // Creating an instance of MyRunnable
+
+        MyRunnable runnable1 = new MyRunnable();
+        Thread thread2 = new Thread(runnable1);     // send this runnable1 as an argument within the constructor
 
     }
 }
