6d611c976edea2ca94e9ea383221273d13f736cf
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-02 14:52:05 +0530

    Converting to Daemon threads

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        // Creating a thread by creating the subclass of a thread class
+        //Method 1 - Creating a thread by creating the subclass of a thread class
         MyThread thread1 = new MyThread();      //Creating an instance of MyThread in the main class.
 
         /* Method 2 - Create an instance of a class that implements the runnable interface
-           and then can pass the instance within the constructor of a thread object. */
-        // Creating an instance of MyRunnable
+       and then can pass the instance within the constructor of a thread object. */
+        //Creating an instance of MyRunnable
 
         MyRunnable runnable1 = new MyRunnable();
         Thread thread2 = new Thread(runnable1);     // send this runnable1 as an argument within the constructor
 
+
+        //Daemon threads are background threads or Non-User threads
+        // thread1.setDaemon(true);
+        // thread2.setDaemon(true);
+
         thread1.start();
+        thread1.join();         //the main thread will wait until thread 1 is finished and it's going to continue with rest of the program.
         thread2.start();
     }
 }
