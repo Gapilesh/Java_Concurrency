aec451605be2ec7581b0c9857a2f06e9ccbefec7
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-02 13:57:09 +0530

    Creating a thread by creating the subclass of a thread class

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Creating a thread
+        //Creating a thread by creating the subclass of a thread class
         MyThread thread1 = new MyThread();      //Creating an instance of MyThread in the main class.
 
 
