4854e576f428330b596bdec0c5ad84f3711e1ce1
Gapilesh Pirabaharan <108779095+Gapilesh@users.noreply.github.com>
2024-12-02 13:09:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
 
+        //Creating a thread
+        MyThread thread1 = new MyThread();      //Creating an instance of MyThread in the main class.
+
+
+    }
+}
